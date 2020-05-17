fa8e3f9ac1d159ba4417e58a1211ad02667404d7
pawelsmiechowski <pksmiechowski@wi.zut.edu.pl>
2020-05-17 18:21:45 +0200

    Tworzenie listy wpisów

--- app/src/main/java/com/example/android_lab_4/MainActivity.java
+++ app/src/main/java/com/example/android_lab_4/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android_lab_4;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] value = new String[] {"Pies","Kot","Chomik"};
+        this.target = new ArrayList<>();
+        this.target.addAll(Arrays.asList(value));
+
+        this.adpater = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        ListView listview=(ListView) findViewById(R.id.listView);
+        listview.setAdapter(this.adpater);
     }
+    private ArrayList<String> target;
+    private ArrayAdapter adpater;
+
+
 }
 //TODO
