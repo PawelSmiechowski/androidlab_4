81791bf2c24433a05718f51c9c6eaf97f9e7b59e
pawelsmiechowski <pksmiechowski@wi.zut.edu.pl>
2020-05-17 18:56:38 +0200

    Obsługa formularza dodawania

--- app/src/main/java/com/example/android_lab_4/MainActivity.java
+++ app/src/main/java/com/example/android_lab_4/MainActivity.java
@@ -38,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
         Intent intencja = new Intent(this, DodajWpis.class);
         startActivityForResult(intencja, 1);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode ,Intent data){
+        if(requestCode==1 && requestCode==RESULT_OK){
+            Bundle extras=data.getExtras();
+            String nowy=(String)extras.get("wpis");
+            target.add(nowy);
+            adpater.notifyDataSetChanged();
+        }
+    }
     private ArrayList<String> target;
     private ArrayAdapter adpater;
 
